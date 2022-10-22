f838358997c4b7a81ce8cd91ef68eba412d3c7b3
Stephanie Page <stephaniepage@stephanies-air.lan>
2022-10-22 10:02:49 -0500

    add DB error handling

--- inventorydemo/libraries/serverlib/stateServer.js
+++ inventorydemo/libraries/serverlib/stateServer.js
@@ -7,7 +7,27 @@ const connectToMongoDb = require('./mongoDb/mongoConnection')
 
 module.exports = async (changeStreamOptions) => {
   const { mongoDbOptions, redisDbOptions } = changeStreamOptions;
+ 
+  try {
   const redis = new Redis(redisDbOptions);
+} catch (err) {
+  if (err) {
+    console.log(`Error occured when connecting to Redis.  errName: ${err.name}, errMessage: ${err.message}, errStack: ${err.stack}`)
+  } else {
+    let redisError = new Error('An error occured when connecting to Redis')
+    console.log(redisError)
+  }
+}
+try {
   const client = await connectToMongoDb(mongoDbOptions.uri)
+  } catch (err) {
+    if (err) {
+      console.log(`Error occured when connecting to MongoDB.  errName: ${err.name}, errMessage: ${err.message}, errStack: ${err.stack}`)
+    } else {
+      let mongoDbError = new Error('An error occured when connecting to MongoDB')
+      console.log(mongoDbError)
+    }
+  }
+
   return {redis, client}
 }
